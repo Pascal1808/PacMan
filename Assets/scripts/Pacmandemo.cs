0f603d03abbbae25b607690277313740daadb679
pascal <39971@ma-web.nl>
2026-02-12 12:31:59 +0100

    Pacmandemo

---
+++ Assets/scripts/Pacmandemo.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Pacmandemo : MonoBehaviour
+{
+   public float speed = 10.0f;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+
+        transform.Translate(movement * speed * Time.deltaTime);       
+    }
+
+
+    
+}
